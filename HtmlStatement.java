3cf14924ad1beed44a32ba112e7f4de3123fa767
Denisgaillac <denisgaillac@ufmg.br>
2021-03-11 10:40:53 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,22 +1,32 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
+    private String createHeader(String name){
+        return "<H1>Rentals for <EM>" + name + "</EM></H1><P>\n";
+    }
+
+    private String createBody(String title, Double charge){
+        return title + ": " + String.valueOf(charge) + "<BR>\n";
+    }
+
+    private String createFooter(double totalCharge, int renterPoints){
+        return "<P>You owe <EM>" + String.valueOf(totalCharge) + "</EM><P>\n" +
+            "On this rental you earned <EM>" + String.valueOf(renterPoints) +
+            "</EM> frequent renter points<P>";
+    }
+    
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-        "</EM></H1><P>\n";
+        String result = createHeader(aCustomer.getName());
         while (rentals.hasMoreElements()) {
            Rental each = (Rental) rentals.nextElement();
-           //show figures for each rental
-           result += each.getMovie().getTitle()+ ": " +
-           String.valueOf(each.getCharge()) + "<BR>\n";
+           //show figures for this rental
+           result += createBody(each.getMovie().getTitle(),
+                each.getCharge());
         }
         //add footer lines
-        result += "<P>You owe <EM>" +
-        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + 
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        "</EM> frequent renter points<P>";
+        result += createFooter(aCustomer.getTotalCharge(),
+            aCustomer.getTotalFrequentRenterPoints());
         return result;
-    }
+     }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,33 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+    private String createHeader(String name){
+        return "Rental Record for " + name +
+        "\n";
+    }
+
+    private String createBody(String title, Double charge){
+        return "\t" + title+ "\t" + String.valueOf(charge) + "\n";
+    }
+
+    private String createFooter(double totalCharge, int renterPoints){
+        return "Amount owed is " + String.valueOf(totalCharge) + "\n" +
+            "You earned " + String.valueOf(renterPoints) 
+            + " frequent renter points";
+    }
+    
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-        "\n";
+        String result = createHeader(aCustomer.getName());
         while (rentals.hasMoreElements()) {
            Rental each = (Rental) rentals.nextElement();
            //show figures for this rental
-           result += "\t" + each.getMovie().getTitle()+ "\t" +
-           String.valueOf(each.getCharge()) + "\n";
+           result += createBody(each.getMovie().getTitle(),
+                each.getCharge());
         }
         //add footer lines
-        result += "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
+        result += createFooter(aCustomer.getTotalCharge(),
+            aCustomer.getTotalFrequentRenterPoints());
         return result;
      }
 }
